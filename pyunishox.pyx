3af7c16e69e6491366ced11a7612463cfe5bc9a5
James Gerity <snoopjedi@gmail.com>
2020-10-27 19:05:21 -0400

    Reinstate type for _in

--- pyunishox.pyx
+++ pyunishox.pyx
@@ -6,12 +6,12 @@ cimport unishox
 
 MAXALLOC = 1024
 
-def compress(_in):
+def compress(char* _in):
     cdef array.array out = array.array('b', [0] * len(_in))  # on the way in, we can safely allocate...
     cdef int N = unishox.unishox1_compress(_in, len(_in), out.data.as_chars, NULL)
     return out[:N].tobytes()
 
-def decompress(_in):
+def decompress(char* _in):
     cdef array.array _inn = array.array('b', _in)
     cdef array.array out = array.array('b', [0] * MAXALLOC)  # on the way out, it's a crapshoot
     cdef int N = unishox.unishox1_decompress(_inn.data.as_chars, len(_in), out.data.as_chars, NULL)
