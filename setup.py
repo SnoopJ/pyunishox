70a3141346427efe2287b264e3cf9b7017bf1d6a
James Gerity <snoopjedi@gmail.com>
2020-11-02 09:03:07 -0500

    Specify Cython build dependency (PEP 518)

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from Cython.Build import cythonize
 setup(
     name="pyunishox",
     license="MIT",
-    version="0.1",
+    version="0.2",
     ext_modules=cythonize([Extension("pyunishox", ["pyunishox.pyx"])], language_level=3)
 )
